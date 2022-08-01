c281e33734c5ed77580d8cb3989ffc1306b656be
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-08-01 10:24:43 -0500

    solution qr component

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                Whiter: 'hsl(0, 0%, 100%)',
+                Light_gray: 'hsl(212, 45%, 89%)',
+                Grayish_blue: 'hsl(220, 15%, 55%)',
+                Dark_blue: 'hsl(218, 44%, 22%)',
+            },
+
+            fontFamily:{
+                Outfit: ['Outfit', 'sans-serif'],
+            }
+        }
+    }
+}
